f9cd365e52d54a33754b4e4779467f495b2aea58
laherasif <laherasif@gmail.com>
2022-09-04 13:58:16 -0700

    ticker data shwo

--- src/components/Homepage/Navbar/Marquee.js
+++ src/components/Homepage/Navbar/Marquee.js
@@ -62,14 +62,45 @@ const MarqueeView = () => {
               <img
                 className="marq-logo"
                 style={{ verticalAlign: "bottom" }}
-                src={`https://hporxadminbackend.herokuapp.com/${ticker.logoFile}`}
-                alt={marqee}
+                src={`http://localhost:5873${ticker.logoFile}`}
+                alt={"aly"}
               />
-              {ticker.tickerText}
             </span>
+            {ticker.tickerText}
           </>
         ))}
-
+        {/* <span style={{ display: "inline-block", margin: "0 1rem" }}>
+          <img
+            className="marq-logo"
+            style={{ verticalAlign: "bottom" }}
+            src={marqee}
+            alt={marqee}
+          />
+        </span>
+        Welcome to Hemp Products Online Therapeutic Pharmaceuticals FinSec Alpha
+        v8.1.7.22 React Development Release Candidate.
+        <span style={{ display: "inline-block", margin: "0 1rem" }}>
+          <img
+            className="marq-logo"
+            style={{ verticalAlign: "bottom" }}
+            src={marqee}
+            alt={marqee}
+          />
+        </span>
+        This Alpha v8.1.7.22 development candidate represents the successful
+        design phalloplasty we intoed to peruse in a module by module
+        development schedule beginning immediately.
+        <span style={{ display: "inline-block", margin: "0 1rem" }}>
+          <img
+            className="marq-logo"
+            style={{ verticalAlign: "bottom" }}
+            src={marqee}
+            alt={marqee}
+          />
+        </span>
+        You are welcome to check back, our design and development process is
+        iterative, it will be a pleasure to surprise you as the development is
+        completed during 2022. */}
       </Marquee>
     </div>
   );
