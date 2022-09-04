45f4a5766d8f502735fce5bbce4dbf2a9ac46ff0
laherasif <laherasif@gmail.com>
2022-09-04 13:45:57 -0700

    add image

--- src/components/Homepage/Navbar/Marquee.js
+++ src/components/Homepage/Navbar/Marquee.js
@@ -4,38 +4,40 @@ import Marquee from "react-fast-marquee";
 import "./offCanvas.css";
 import marqee from "../../../assets/tickerlogo.png";
 import axios from "axios";
+import { useState } from "react";
 
 const MarqueeView = () => {
 
-
+  const [tickers, setTickers] = useState([])
   useEffect(() => {
     (async () => {
-        try {
+      try {
 
-          let payload = {
+        let payload = {
 
-                "sortproperty": "created_at",
-                "sortorder": -1,
-                "offset": 0,
-                "limit": 2,
-                "query": {
-                    "critarion": {"active" : true},
-                    
-                    "addedby": "_id email first_name",
-                    
-                    "lastModifiedBy": "_id email first_name"
-                }
-            
-            }
-           
-            let response = await axios.post("https://hporxadminbackend.herokuapp.com/tickers/getTickersWithFullDetailsPublic", payload);
-            // setallpermission(response.data.permissions)
-            console.log("ticker" , response)
-        } catch (error) {
-            console.log(error);
+          "sortproperty": "created_at",
+          "sortorder": -1,
+          "offset": 0,
+          "limit": 2,
+          "query": {
+            "critarion": { "active": true },
+
+            "addedby": "_id email first_name",
+
+            "lastModifiedBy": "_id email first_name"
+          }
+
         }
+
+        let response = await axios.post("https://hporxadminbackend.herokuapp.com/tickers/getTickersWithFullDetailsPublic", payload);
+        // setallpermission(response.data.permissions)
+        setTickers(response.data.data.tickers)
+        console.log("ticker", response)
+      } catch (error) {
+        console.log(error);
+      }
     })();
-}, [])
+  }, [])
 
 
   return (
@@ -54,38 +56,20 @@ const MarqueeView = () => {
         direction="left"
         speed="70"
       >
-        <span style={{ display: "inline-block", margin: "0 1rem" }}>
-          <img
-            className="marq-logo"
-            style={{ verticalAlign: "bottom" }}
-            src={marqee}
-            alt={marqee}
-          />
-        </span>
-        Welcome to Hemp Products Online Therapeutic Pharmaceuticals FinSec Alpha
-        v8.1.7.22 React Development Release Candidate.
-        <span style={{ display: "inline-block", margin: "0 1rem" }}>
-          <img
-            className="marq-logo"
-            style={{ verticalAlign: "bottom" }}
-            src={marqee}
-            alt={marqee}
-          />
-        </span>
-        This Alpha v8.1.7.22 development candidate represents the successful
-        design phalloplasty we intoed to peruse in a module by module
-        development schedule beginning immediately.
-        <span style={{ display: "inline-block", margin: "0 1rem" }}>
-          <img
-            className="marq-logo"
-            style={{ verticalAlign: "bottom" }}
-            src={marqee}
-            alt={marqee}
-          />
-        </span>
-        You are welcome to check back, our design and development process is
-        iterative, it will be a pleasure to surprise you as the development is
-        completed during 2022.
+        {tickers.map((ticker, i) => (
+          <>
+            <span style={{ display: "inline-block", margin: "0 1rem" }}>
+              <img
+                className="marq-logo"
+                style={{ verticalAlign: "bottom" }}
+                src={`https://hporxadminbackend.herokuapp.com/${ticker.logoFile}`}
+                alt={marqee}
+              />
+              {ticker.tickerText}
+            </span>
+          </>
+        ))}
+
       </Marquee>
     </div>
   );
