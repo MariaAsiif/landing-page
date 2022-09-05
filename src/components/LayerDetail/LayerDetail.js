380341886791edb58e635ee423b42e074b5df216
laherasif <laherasif@gmail.com>
2022-09-05 02:25:29 -0700

    add quote

--- src/components/LayerDetail/LayerDetail.js
+++ src/components/LayerDetail/LayerDetail.js
@@ -4,12 +4,14 @@ import HomePageNav from "../Homepage2/HomePageNav";
 import AgenciesHeader from "../AgenciesPage/AgenciesHeader/AgenciesHeader";
 import Detail from "./Detail";
 import FeedBack from "../Feedback/FeedBack";
+import Quote from "../Homepage/Quote/Quote";
 function LayerDetail() {
   return (
     <>
       <AgenciesHeader />
       <HomePageNav />
       <Detail />
+      <Quote/>
       <FeedBack/>
     </>
   );
